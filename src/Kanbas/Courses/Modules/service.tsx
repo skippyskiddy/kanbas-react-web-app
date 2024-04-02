4ca8585b895206133afe4dd26fabe71951edb481
Elif <elifdaa@gmail.com>
2024-04-01 20:24:46 -0400

    pre deploy

--- src/Kanbas/Courses/Modules/service.tsx
+++ src/Kanbas/Courses/Modules/service.tsx
@@ -3,6 +3,7 @@ const COURSES_API = "http://localhost:4000/api/courses";
 
 
 const MODULES_API = "http://localhost:4000/api/modules";
+
 export const deleteModule = async (moduleId:any) => {
   const response = await axios
     .delete(`${MODULES_API}/${moduleId}`);
